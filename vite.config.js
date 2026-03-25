f02740e9c45dd74d6a5cfcfbbc0faf7bcdcb91ea
ajshankar1 <ajaishankar83@gmail.com>
2026-03-25 11:59:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: Replace 'igs-booking-system' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/igs-booking-system/',
+  base: './',
 })
